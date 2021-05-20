5ad2ecdd67a53b094ce4e0c91609b71a3a1f66f2
renato.a.santana <renato.a.santana@accenture.com>
2021-05-20 08:38:46 -0300

    Starters + First controller

---
+++ src/main/java/org/renatosantana/VendasApplication.java
@@ -0,0 +1,20 @@
+package org.renatosantana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class VendasApplication {
+
+    @GetMapping("/hello")
+    String helloWorld(){
+        return "Hello world!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
