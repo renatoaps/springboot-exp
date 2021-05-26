ea002cc18cded099c8f7d62849b7f00d8b6a7b9a
Renato Santana <renato.dev@icloud.com>
2021-05-26 06:42:49 -0300

    Finished CRUD using jdbctemplate.

--- src/main/java/org/renatosantana/VendasApplication.java
+++ src/main/java/org/renatosantana/VendasApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @SpringBootApplication
 public class VendasApplication {
@@ -17,11 +19,33 @@ public class VendasApplication {
     public CommandLineRunner init(@Autowired Clientes clientes){
 
         return args -> {
+          System.out.println(":: Salvando Clientes");
           clientes.salvarCliente(new Cliente("Renato"));
           clientes.salvarCliente(new Cliente("Jessica"));
           clientes.salvarCliente(new Cliente("Cartório Safado"));
 
-            System.out.println(clientes.obterTodos().toString());
+          System.out.println(":: Listando Clientes");
+          List<Cliente> obterTodos = clientes.obterTodos();
+          obterTodos.forEach(System.out::println);
+
+          obterTodos.forEach(c -> {
+            c.setNome(c.getNome() + " atualizado");
+            clientes.atualizarCliente(c);
+          });
+
+          System.out.println(":: Atualizando Clientes ::");
+          obterTodos.forEach(System.out::println);
+
+          System.out.println(":: Buscando Cliente::");
+          List<Cliente> busca = clientes.obterPorNome("Jess");
+          busca.forEach(System.out::println);
+
+          System.out.println(":: Cliente Deletado ::");
+          clientes.deletarCliente(3);
+
+          System.out.println(":: Limpando tabela um por um, porque sim::");
+          obterTodos.forEach(c -> clientes.deletarCliente(c.getId()));
+
         };
     }
 
